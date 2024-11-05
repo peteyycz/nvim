3de81660e68d86230aacde733bd9d8a096506a56
Peter Czibik <dev@peterczibik.com>
2024-11-05 14:09:32 +0100

    Replace go with builtin plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,8 +19,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-
+    { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "plugins" },
   },
   defaults = {
