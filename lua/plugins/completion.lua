6d36b0ca9dea7baa0dd771dfcde3a07873be16e4
Peter Czibik <dev@peterczibik.com>
2025-01-04 20:22:20 +0100

    Default mapping is better for cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,7 +7,7 @@ return {
     ---@type blink.cmp.Config
     opts = {
       signature = { enabled = true },
-      keymap = { preset = 'enter' },
+      keymap = { preset = 'default' },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
