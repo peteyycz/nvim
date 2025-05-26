fa4efcb2c12fb63d62b72aa0a65c4a221438135a
Peter Czibik <p.czibik@gmail.com>
2025-05-26 13:53:42 +0200

    Update theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,17 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
+    -- config = function()
+    --   vim.cmd.colorscheme "gruvbox"
+    -- end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
     config = function()
-      vim.cmd.colorscheme "gruvbox"
+      vim.cmd.colorscheme "tokyonight"
     end,
   },
   {
