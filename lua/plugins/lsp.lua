b9f5044bf5937bd35bad8892f8f0a5e9c9a5dcf9
Peter Czibik <p.czibik@gmail.com>
2025-06-11 15:57:57 +0200

    Oil fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
+      automatic_enable = false,
       ensure_installed = { "lua_ls", "vtsls", "elixirls", "eslint", "rust_analyzer", "clangd", "templ", "gopls" },
     },
   },
